9deecc5f59934f83f546c9850140d1db9fd599b1
Arch me <kishudwivedi30@gmail.com>
2025-09-06 08:40:21 +0530

    labelling loop

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -7,15 +7,53 @@
 //        }
 //    }
 
+//    fn main() {
+//        let number = 6;
+//        if number % 4 == 0 {
+//            println!("the number is divisible by 4");
+//        } else if number % 3 == 0 {
+//            println!("the number is divisible by 3");
+//        } else if number % 2 == 0 {
+//            println!("the number is divisible by 2");
+//        } else {
+//            println!("the number is not divisible by 2, 3, 4");
+//        }
+//    }
+
+//    fn main() {
+//        loop {
+//            println!("again!");
+//        }
+//    }
+
+//    fn main() {
+//        let mut counter = 0;
+//        let result = loop {
+//            counter += 1;
+//            if counter == 10 {
+//                break counter * 2;
+//            }
+//        };
+//        println!("result: {result}, counter: {counter}");
+//    }
+
 fn main() {
-    let number = 6;
-    if number % 4 == 0 {
-        println!("the number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("the number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("the number is divisible by 2");
-    } else {
-        println!("the number is not divisible by 2, 3, 4");
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining: {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
     }
+    println!("End count = {count}");
 }
