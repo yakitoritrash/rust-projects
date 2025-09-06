2e98275eca5292499b0919c0c383c620b0928524
Arch me <kishudwivedi30@gmail.com>
2025-09-06 06:04:09 +0530

    expression

--- function/src/main.rs
+++ function/src/main.rs
@@ -1,8 +1,16 @@
-fn main() {
-    println!("Hello, world!");
-    another_function(5);
+//    fn main() {
+//        println!("Hello, world!");
+//        another_function(5, 'c');
+//    }
+
+fn another_function(x: i32, unit_label: char) {
+    println!("value of x is {x}{unit_label}");
 }
 
-fn another_function(x: i32) {
-    println!("value of x is {x}");
+fn main() {
+    let y = {
+        let x = 6;
+        x
+    };
+    println!("value of y: {y}");
 }
