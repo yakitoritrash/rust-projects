7bd550e280192efcca2f3a229c475087f67c7856
Arch me <kishudwivedi30@gmail.com>
2025-09-08 20:59:06 +0530

    move string

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     //let _s = "hello";
-    let mut s = String::from("hello");
-    s.push_str(", world!");
-    println!("{s}")
+    let mut s1 = String::from("hello");
+    s1.push_str(", world!");
+    let s2 = s1;
+    println!("{s2}");
 }
