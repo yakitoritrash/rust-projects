781eb118a412acdc7d89baee760184754e0413d4
Arch me <kishudwivedi30@gmail.com>
2025-09-09 15:16:32 +0530

    .clone()

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     let mut s1 = String::from("hello");
     s1.push_str(", world!");
     //let s2 = s1;
+    let s2 = s1.clone();
     s1 = String::from("ahoy");
-    println!("{s1}");
+    println!("{s1} {s2}");
 }
