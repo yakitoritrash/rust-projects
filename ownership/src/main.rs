801118a4edbf8859c1be89c352699c8f47fe669f
Arch me <kishudwivedi30@gmail.com>
2025-09-11 23:02:30 +0530

    references

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -18,7 +18,9 @@ fn main() {
 
     let mut s = String::from("hello");
     //change(&mut s);
-    let r1 = &mut s;
+    {
+        let r1 = &mut s;
+    }
     let r2 = &mut s;
     println!("{r1} {r2}");
 }
